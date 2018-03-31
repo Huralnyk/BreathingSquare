070bd11776cb78a12d259d57a5905014a32b2333
Oleksii Huralnyk <ohuralnyk@kaaiot.io>
2018-03-31 09:35:11 +0300

    Layout view

---
+++ BreathingSquare/BreathingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BreathingViewController.swift
+//  BreathingSquare
+//
+//  Created by Oleksii Huralnyk on 3/31/18.
+//  Copyright © 2018 Oleksii Huralnyk. All rights reserved.
+//
+
+import UIKit
+
+class BreathingViewController: UIViewController {
+    
+    @IBOutlet weak var breathingView: UIView?
+    @IBOutlet weak var promptLabel: UILabel?
+    @IBOutlet weak var phaseTypeLabel: UILabel?
+    @IBOutlet weak var currentPhaseTimerLabel: UILabel?
+    @IBOutlet weak var breathingCycleTimerLabel: UILabel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
